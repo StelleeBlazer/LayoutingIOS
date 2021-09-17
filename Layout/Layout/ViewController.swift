7433de1a746b3a9eadeab5b924de9c5c68e87def
Rahmananda <55073796+rahmanandas@users.noreply.github.com>
2021-09-17 15:04:27 +0700

    Add files via upload

---
+++ Layout/Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Layout
+//
+//  Created by Mac n Cheese on 07/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
